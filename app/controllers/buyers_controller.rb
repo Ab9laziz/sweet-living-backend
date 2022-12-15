636fca85da9193a166e6cd3709e1d2e0b3b72bd2
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-16 01:01:55 +0300

    buyer controller operations

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,26 @@
 class BuyersController < ApplicationController
+    def index 
+        render json: Buyer.all
+    end
+
+    def create
+        buyer = Buyer.create!(buyer_params)
+        render json: buyer
+    end
+
+    def update
+        buyer = find_buyer
+        buyer.update!(find_buyer)
+        render json: buyer
+    end
+
+    private
+
+    def find_buyer
+        Buyer.find_by!(id: params[:id])
+    end
+
+    def buyer_params
+        params.permit(:username, :email, :password_digest)
+    end 
 end
