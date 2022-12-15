7fed93f5ace1c27458a8236594e6395ea53183cc
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-16 00:57:05 +0300

    add property crud operations

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,31 @@
 class PropertiesController < ApplicationController
+    def index 
+        render json: Property.all
+    end
+
+    def show
+        property = find_property
+        render json: property
+    end
+
+    def destroy
+        property = Property.find(params[:id])
+        property.destroy
+        head :no_content
+    end
+
+    def update 
+        property = Property.find(params[:id])
+        property.update!(property_params)
+        render json: property
+    end
+
+    private 
+    def find_property
+        Buyer.find_by!(id: params[:id])
+    end
+    
+    def property_params
+        params.permit(:houses, :image_url, :location, :number_of_rooms, :seller_id, :buyer_id)
+    end
 end
