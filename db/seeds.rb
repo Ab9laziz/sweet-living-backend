6dc2f1c78db86fed5b846e069cc37c2ba89d7238
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-12-16 08:38:59 +0300

    FH-8 adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "🌱 Seeding sweet living data..."
+
+
+
+Buyer.create(username: "John", email: "john@gmail.com")
+
+#create data for table Property
+
+Property.create(houses: "3 bedroom flat", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.realtor.com%2Fadvice%2Fhome-improvement%2Fhow-to-stage-a-house%2F&psig=AOv", location: "Lagos", number_of_rooms: 3, seller_id: 1, buyer_id: 1)  
+Property.create(houses: "2 bedroom flat", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.realtor.com%2Fadvice%2Fhome-improvement%2Fhow-to-stage-a-house%2F&psig=AOv", location: "Lagos", number_of_rooms: 2, seller_id: 1, buyer_id: 1)
+Property.create(houses: "1 bedroom flat", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.realtor.com%2Fadvice%2Fhome-improvement%2Fhow-to-stage-a-house%2F&psig=AOv", location: "Lagos", number_of_rooms: 1, seller_id: 1, buyer_id: 1)
+Property.create(houses: "4 bedroom flat", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.realtor.com%2Fadvice%2Fhome-improvement%2Fhow-to-stage-a-house%2F&psig=AOv", location: "Lagos", number_of_rooms: 4, seller_id: 1, buyer_id: 1)
+Property.create(houses: "5 bedroom flat", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.realtor.com%2Fadvice%2Fhome-improvement%2Fhow-to-stage-a-house%2F&psig=AOv", location: "Lagos", number_of_rooms: 5, seller_id: 1, buyer_id: 1)
+Property.create(houses: "6 bedroom flat", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.realtor.com%2Fadvice%2Fhome-improvement%2Fhow-to-stage-a-house%2F&psig=AOv", location: "Lagos", number_of_rooms: 6, seller_id: 1, buyer_id: 1)
+Property.create(houses: "7 bedroom flat", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.realtor.com%2Fadvice%2Fhome-improvement%2Fhow-to-stage-a-house%2F&psig=AOv", location: "Lagos", number_of_rooms: 7, seller_id: 1, buyer_id: 1)
+
+#create data for table Seller
+    
+Seller.create(username: "John", email: "john@gmail.com    ",  phone_number: 1234567890)
