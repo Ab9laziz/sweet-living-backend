2c314930ecedfbfec8f09bf4901808b6b660ae45
codertinie <martin.munyao@moringaschool.com>
2022-12-19 13:45:24 +0300

    Added price, category and description columns in properties table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_201206) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_104129) do
   create_table "buyers", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -28,6 +28,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_201206) do
     t.integer "buyer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
+    t.string "category"
+    t.string "description"
   end
 
   create_table "sellers", force: :cascade do |t|
